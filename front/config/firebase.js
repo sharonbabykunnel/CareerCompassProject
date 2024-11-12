7b7684b96431330abfba29561faf18b2d21a7512
Sharon <sharonkunnel2003@gmail.com>
2024-11-12 21:52:10 +0530

    12.11.1

--- front/config/firebase.js
+++ front/config/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUAcgcvZauf85JG2iN6s2lom0M7Hm-9Sk",
-  authDomain: "careercompass-a9b5b.firebaseapp.com",
-  projectId: "careercompass-a9b5b",
-  storageBucket: "careercompass-a9b5b.appspot.com",
-  messagingSenderId: "490981674126",
-  appId: "1:490981674126:web:5b9f7e8ce4193011e48ddf",
-  measurementId: "G-Q9V9EJ1M0F",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
