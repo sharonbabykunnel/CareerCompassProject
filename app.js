cd07003ccf534e8a245e570c7f1965fd0cbae765
Sharon <sharonkunnel2003@gmail.com>
2024-12-17 10:31:05 +0530

    17.12.6

--- app.js
+++ app.js
@@ -30,8 +30,24 @@ app.use(
 );
 app.options('*', cors());
 const peerServer = ExpressPeerServer(server, { debug: true });
-app.use('/api', routes);
+app.use("/api", routes);
 app.use("/peerjs", peerServer);
+
+// Serve assets with correct MIME types
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "front", "dist", "assets"), {
+    setHeaders: (res, path) => {
+      if (path.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
+      } else if (path.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      }
+    },
+  })
+);
+
+// Serve other static files
 app.use(
   express.static(path.join(__dirname, "front", "dist"), {
     setHeaders: (res, path) => {
