d3d533fa8b5ee36809d6ae589122604891806a83
Sharon <sharonkunnel2003@gmail.com>
2024-12-21 10:43:35 +0530

    21.12.2

--- backEnd/utils/access_token.js
+++ backEnd/utils/access_token.js
@@ -2,7 +2,7 @@ import JWT from 'jsonwebtoken'
 import config from './../../config.js'
 
 const accessToken = (uid) => {
-    return JWT.sign({uid},config.access_secret,{expiresIn:'15M'})
+    return JWT.sign({uid},config.access_secret,{expiresIn:'15m'})
 }
 
 export default accessToken;
---
+++ front/dist/assets/arrow-big-left-5KbGk9Bl.js
@@ -0,0 +1,6 @@
+import{e as r}from"./index-yZXWRwRE.js";/**
+ * @license lucide-react v0.411.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=r("ArrowBigLeft",[["path",{d:"M18 15h-6v4l-7-7 7-7v4h6v6z",key:"lbrdak"}]]);export{o as A};
